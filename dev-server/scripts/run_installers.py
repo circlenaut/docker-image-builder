048d41009ce60de7ca55c9678f2c5bec8811eaaa
Phillip Strefling <phillip@proneer.co>
2021-02-11 03:01:02 +0000

    fixed installer check

--- dev-server/scripts/run_installers.py
+++ dev-server/scripts/run_installers.py
@@ -21,8 +21,8 @@ import functions as func
 def install_script(path):
     return_code = -1
     tracker = dict()
+    tracker['time'] = datetime.datetime.now().strftime("%Y-%m-%d-%H:%M")
     tracker['file'] = os.path.basename(path)
-    tracker['time'] = datetime.datetime.now().ctime()
     tracker['status'] = str()
     tracker['code'] = int()
     split = p.split(".", maxsplit=1)
@@ -85,48 +85,59 @@ workspace_dir = os.path.normpath(cli_user.get("dirs").get("workspace").get("path
 data_dir = os.path.normpath(cli_user.get("dirs").get("data").get("path"))
 
 ### Run custom user install scripts
-install_tracker = dict()
-install_tracker_json = dict()
 install_dir = os.path.join(workspace_dir, "installers")
 install_tracker_path = os.path.join(install_dir, "history")
 
-log.critical(os.path.exists(install_tracker_path))
-log.critical(func.is_json(install_tracker_path))
-#log.critical(json.loads(install_tracker_path))
-
-if os.path.exists(install_tracker_path) and func.is_json(install_tracker_path):
+if os.path.exists(install_tracker_path):
     log.debug(f"installation history exists at: '{install_tracker_path}'")
-    install_tracker_file = func.read_file(install_tracker_path)
-    install_tracker_json = json.loads(install_tracker_file)
+    installer_history = func.read_file(install_tracker_path)
+else:
+    installer_history = list()
 if os.path.exists(install_dir):
     log.debug(f"installation dir exists: '{install_dir}'")
     installers = os.listdir(install_dir)
     for p in installers:
+        install_tracker = dict()
+        previous = None
         installer_path = os.path.join(install_dir, p) 
         if os.path.isdir(installer_path) or not os.path.isfile(installer_path):
             continue
+        elif p == 'history':
+            continue
         if install_tracker.get(installer_path) == None:
             install_tracker[installer_path] = dict()
-        previous = install_tracker_json.get(installer_path)
-        log.critical(install_tracker_json)
-        log.debug(f"previous install: '{previous}'")
+        for l in installer_history:
+            if l.isspace():
+                log.debug(f"empty line")
+                continue
+            elements = l.split(" ")
+            if len(elements) == 4:
+                if elements[1] == p:
+                    previous = elements[2]   
+            else:
+                log.error(f"invalid line: '{l}'")
         if previous == None:
             install_tracker[installer_path] = install_script(installer_path)
-        elif previous.get("status") == 'failed':
+        elif previous == 'failed':
+            log.warning(f"script previously failed, retrying: '{installer_path}'")
             install_tracker[installer_path] = install_script(installer_path)
-        elif previous.get("status") == 'installed':
+        elif previous == 'installed':
             log.warning(f"script previously installed: '{installer_path}'")
             install_tracker[installer_path]['status'] = 'installed'
             continue
         else:
             log.error("unknown error")
             sys.exit()
-    if len(install_tracker) == 0:
-        log.info(f"no installation scripts found in '{install_dir}'")
-    # Write config file
-    installer_json = json.dumps(install_tracker, indent = 4)
 
-    with open(install_tracker_path, "w") as f: 
-        f.write(installer_json)
+        if len(install_tracker[installer_path]) == 0:
+            log.info(f"no action taken for script '{installer_path}'")
+            continue
+        elif len(install_tracker[installer_path]) == 4:
+            s = " "
+            status = [str(i) for i in install_tracker[installer_path].values()]
+            with open(install_tracker_path, "a") as f: 
+                f.write(s.join(status) + "\n")
+        else:
+            log.info(f"invalid format for '{installer_path}'")     
 else:
     log.info(f"installer dir does not exist: '{install_dir}'")
