a2e61b767739d4f669dfc06c8401f1a52285b80d
Phillip Strefling <phillip@proneer.co>
2021-02-11 05:15:02 +0000

    magento

---
+++ magento-demo/build.sh
@@ -0,0 +1,12 @@
+sudo docker-compose down
+sudo docker volume rm magento_cache_data
+sudo docker volume rm magento_db_data
+sudo docker volume rm magento_proxy_data
+#sudo docker volume rm magento_dev_home
+sudo docker volume rm magento_dev_workspace
+sudo docker volume rm magento_dev_data
+sudo docker volume rm magento_dev_apps
+sudo docker volume list
+sudo python3 ../Image-Builder/build.py --config build.yml --log_level debug --gzip --overwrite
+sudo docker-compose up -d
+sudo docker logs -f magento_dev
