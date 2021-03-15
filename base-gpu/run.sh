9e4ddaf055ca4a84a4830e3e0fc645ae6978694b
Phillip Strefling <phillip@proneer.co>
2021-03-15 22:30:43 +0000

    fixed image command

--- base-gpu/run.sh
+++ base-gpu/run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-#sudo docker container run -it cuda:10.1-cudnn7-devel-ubuntu18.04 /bin/sh
 PS3='Choose which version to run: '
 options=(
     "Ubuntu 18.04 with Cuda 10.1 and cuDNN7" \
@@ -14,22 +13,38 @@ select opt in "${options[@]}"; do
     case $opt in
         "Ubuntu 18.04 with Cuda 10.1 and cuDNN7")
             echo "Running image: $opt"
-	        sudo docker container run --name bionic_cuda_10-1_cudnn7 --rm registry.dev.proneer.co/cuda:10.1-cudnn7-devel-ubuntu18.04
+            if [ "$1" == "-i" ]; then
+	            sudo docker container run --name bionic_cuda_10-1_cudnn7 -it --rm --gpus all registry.dev.proneer.co/cuda:10.1-cudnn7-devel-ubuntu18.04
+            else
+                sudo docker container run --name bionic_cuda_10-1_cudnn7 --rm --gpus all registry.dev.proneer.co/cuda:10.1-cudnn7-devel-ubuntu18.04
+            fi
             break
             ;;
         "Ubuntu 18.04 with Cuda 10.2 and cuDNN7")
             echo "Running image: $opt"
-	        sudo docker container run --name bionic_cuda_10-2_cudnn7 --rm registry.dev.proneer.co/cuda:10.2-cudnn7-devel-ubuntu18.04
+            if [ "$1" == "-i" ]; then
+	            sudo docker container run --name bionic_cuda_10-2_cudnn7 -it --rm --gpus all registry.dev.proneer.co/cuda:10.2-cudnn7-devel-ubuntu18.04
+            else
+                sudo docker container run --name bionic_cuda_10-2_cudnn7 --rm --gpus all registry.dev.proneer.co/cuda:10.2-cudnn7-devel-ubuntu18.04
+            fi
             break
             ;;
         "Ubuntu 20.04 with Cuda 11.0 and cuDNN8")
             echo "Running image: $opt"
-	        sudo docker container run --name focal_cuda_11-0_cudnn8 --rm registry.dev.proneer.co/cuda:11.0-cudnn8-devel-ubuntu20.04
+            if [ "$1" == "-i" ]; then
+                sudo docker container run --name focal_cuda_11-0_cudnn8 -it --rm --gpus all registry.dev.proneer.co/cuda:11.0-cudnn8-devel-ubuntu20.04 /bin/bash
+            else
+	            sudo docker container run --name focal_cuda_11-0_cudnn8 --rm --gpus all registry.dev.proneer.co/cuda:11.0-cudnn8-devel-ubuntu20.04
+            fi
             break
             ;;
         "Ubuntu 20.04 with Cuda 11.1 and cuDNN8")
             echo "Running image: $opt"
-	        sudo docker container run --name focal_cuda_11-1_cudnn8 --rm registry.dev.proneer.co/cuda:11.1-cudnn8-devel-ubuntu20.04
+            if [ "$1" == "-i" ]; then
+	            sudo docker container run --name focal_cuda_11-1_cudnn8 -it --rm --gpus all registry.dev.proneer.co/cuda:11.1-cudnn8-devel-ubuntu20.04
+            else
+                sudo docker container run --name focal_cuda_11-1_cudnn8 --rm --gpus all registry.dev.proneer.co/cuda:11.1-cudnn8-devel-ubuntu20.04
+            fi
 	        break
             ;;
         "Quit")
