b2f254bb52ee16d335ae52a3df311b9b8fcd24e8
Phillip Strefling <phillip@proneer.co>
2021-03-16 20:27:53 +0000

    improved handling of tagging and added flashlight image

--- base-gpu/run.sh
+++ base-gpu/run.sh
@@ -4,8 +4,9 @@ PS3='Choose which version to run: '
 options=(
     "Ubuntu 18.04 with Cuda 10.1 and cuDNN7" \
     "Ubuntu 18.04 with Cuda 10.2 and cuDNN7" \
-    "Ubuntu 20.04 with Cuda 11.0 and cuDNN8" \
-    "Ubuntu 20.04 with Cuda 11.1 and cuDNN8" \
+    "Ubuntu 20.04 with Cuda 11.0.3 and cuDNN8" \
+    "Ubuntu 20.04 with Cuda 11.1.1 and cuDNN8" \
+    "Ubuntu 20.04 with Cuda 11.2.1 and cuDNN8" \
     "Quit" \
 )
 COLUMNS=1000
@@ -29,21 +30,30 @@ select opt in "${options[@]}"; do
             fi
             break
             ;;
-        "Ubuntu 20.04 with Cuda 11.0 and cuDNN8")
+        "Ubuntu 20.04 with Cuda 11.0.3 and cuDNN8")
             echo "Running image: $opt"
             if [ "$1" == "-i" ]; then
-                sudo docker container run --name focal_cuda_11-0_cudnn8 -it --rm --gpus all registry.dev.proneer.co/cuda:11.0-cudnn8-devel-ubuntu20.04 /bin/bash
+                sudo docker container run --name focal_cuda_11-0-3_cudnn8 -it --rm --gpus all registry.dev.proneer.co/cuda:11.0.3-cudnn8-devel-ubuntu20.04 /bin/bash
             else
-	            sudo docker container run --name focal_cuda_11-0_cudnn8 --rm --gpus all registry.dev.proneer.co/cuda:11.0-cudnn8-devel-ubuntu20.04
+	            sudo docker container run --name focal_cuda_11-0-3_cudnn8 --rm --gpus all registry.dev.proneer.co/cuda:11.0.3-cudnn8-devel-ubuntu20.04
             fi
             break
             ;;
-        "Ubuntu 20.04 with Cuda 11.1 and cuDNN8")
+        "Ubuntu 20.04 with Cuda 11.1.1 and cuDNN8")
             echo "Running image: $opt"
             if [ "$1" == "-i" ]; then
-	            sudo docker container run --name focal_cuda_11-1_cudnn8 -it --rm --gpus all registry.dev.proneer.co/cuda:11.1-cudnn8-devel-ubuntu20.04
+	            sudo docker container run --name focal_cuda_11-1-1_cudnn8 -it --rm --gpus all registry.dev.proneer.co/cuda:11.1.1-cudnn8-devel-ubuntu20.04
             else
-                sudo docker container run --name focal_cuda_11-1_cudnn8 --rm --gpus all registry.dev.proneer.co/cuda:11.1-cudnn8-devel-ubuntu20.04
+                sudo docker container run --name focal_cuda_11-1-1_cudnn8 --rm --gpus all registry.dev.proneer.co/cuda:11.1.1-cudnn8-devel-ubuntu20.04
+            fi
+	        break
+            ;;
+        "Ubuntu 20.04 with Cuda 11.2.1 and cuDNN8")
+            echo "Running image: $opt"
+            if [ "$1" == "-i" ]; then
+	            sudo docker container run --name focal_cuda_11-2-1_cudnn8 -it --rm --gpus all registry.dev.proneer.co/cuda:11.2.1-cudnn8-devel-ubuntu20.04
+            else
+                sudo docker container run --name focal_cuda_11-2-1_cudnn8 --rm --gpus all registry.dev.proneer.co/cuda:11.2.1-cudnn8-devel-ubuntu20.04
             fi
 	        break
             ;;
